d157ee65f4f8c0846c6894b8fcdaa84f80321afc
Bekbolot <bekabazarkulov265@gmail.com>
2026-02-22 15:41:43 +0600

    dz8

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,32 +1,12 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const Posts = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://dummyjson.com/posts?limit=5')
-      .then(response => {
-        setPosts(response.data.posts);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
-
+const About = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-6">Посты</h1>
-      <div className="space-y-6">
-        {posts.map(post => (
-          <div key={post.id} className="border-b pb-4">
-            <h2 className="text-xl font-semibold mb-2">{post.title}</h2>
-            <p className="text-gray-700">{post.body}</p>
-          </div>
-        ))}
-      </div>
+      <h1 className="text-2xl font-bold mb-4">О нас</h1>
+      <p className="text-gray-700">
+        Это последнее дз 5 месяца
+      </p>
     </div>
   );
 };
 
-export default Posts;
+export default About;
